ed069de73c155275a72e1a5c47cf2ef35460b282
realioseb <soso.bichinashvili@gmail.com>
2014-05-16 17:01:10 +0400

    deleting old #11
    moving to silex framework

---
+++ web/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+// required files
+require __DIR__.'/../app/config.php';
+require __DIR__.'/../app/app.php';
+require __DIR__.'/../lib/db_functions.php';
+
+// routing
+$app->mount('/', new \Dashboard\DashboardModule());
+
+$app->run();
