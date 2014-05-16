41520e921e96f6f9508bdc3dd6b1f48c3dd7a4aa
realioseb <soso.bichinashvili@gmail.com>
2014-05-16 17:03:30 +0400

    adding new #11
    moving to silex

---
+++ app/app.php
@@ -0,0 +1,12 @@
+<?php
+
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider());
+$app->register(new Silex\Provider\FormServiceProvider());
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
+$app->register(new Silex\Provider\DoctrineServiceProvider());
+$app->register(new Silex\Provider\ServiceControllerServiceProvider());
+
+$app->register(new \Dashboard\DashboardModule());
