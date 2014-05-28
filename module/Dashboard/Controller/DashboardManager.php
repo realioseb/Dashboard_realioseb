69ebba0edcee9e1e9227db7d7b0447ce0a713d17
ioseb <i_bichinashvili@cu.edu.ge>
2014-05-28 14:46:24 +0400

    Registration improvements #5
    Added validation functions + changed php responses

--- module/Dashboard/Controller/DashboardManager.php
+++ module/Dashboard/Controller/DashboardManager.php
@@ -20,7 +20,7 @@ class DashboardManager
     {
         //user_id must be the current user logged in
         $user_id = 1;
-        $widgets = \db_functions::db_select('custom_widget', array('user_id' => $user_id), 'and');
+        $widgets = $app['database.abstraction']->db_select('custom_widget', array('user_id' => $user_id), $app);
         
         return $app['twig']->render('@Dashboard/dashboard.twig', array(
             'widgets' => $widgets,
@@ -31,18 +31,22 @@ class DashboardManager
     {
         $data = array();
         
-        $status = false;
+        if (!empty($app['database.abstraction']->db_select('users', array('username' => $_POST['uname']), $app))) {
+            return json_encode(2);
+        }
         
-        if($_POST['checkPassword'] == $user['password'] = $_POST['password']) {
+        if($_POST['checkPassword'] == $_POST['password']) {
             $data['username'] = $_POST['uname'];
             $data['email'] = $_POST['mail'];
             $data['password'] = md5($_POST['password']);
+
+            $app['database.abstraction']->db_insert('users', $data, $app);
             
-            \db_functions::db_insert('users', $data);
-            
-            $status = true;
+            return json_encode(1);
+        } else {
+            return json_encode(3);
         }
         
-        return json_encode($status);
+        return json_encode(null);
     }
 }
--- web/dist/js/myJavascript.js
+++ web/dist/js/myJavascript.js
@@ -1,3 +1,5 @@
+var registration = document.getElementById("reg-response").innerHTML;
+
 // minimize and maximize left sidebar
 function sideBarMaximizeMinimize(evt) {
     var list = evt.parentNode.children;
@@ -11,10 +13,64 @@ function sideBarMaximizeMinimize(evt) {
     }
 }
 
+// check username input
+function checkUsername(evt) {
+    var value = evt.value;
+    
+    var check = value.match(/^[a-z0-9_-]{4,20}$/);
+    
+    if(check) {
+        evt.style.border = "1px solid green";
+    } else {
+        evt.style.border = "1px solid red";
+    }
+}
+
+// check email input
+function checkMail(evt) {
+    var value = evt.value;
+    
+    var check = value.match(/^([a-zA-Z0-9._-]+)[@](\w+)[.](\w+)$/);
+    
+    if(check) {
+        evt.style.border = "1px solid green";
+    } else {
+        evt.style.border = "1px solid red";
+    }
+}
+
+// check password input
+function checkPassword(evt) {
+    var value = evt.value;
+    
+    var check = value.match(/^.{8,64}$/);
+    
+    if(check) {
+        evt.style.border = "1px solid green";
+    } else {
+        evt.style.border = "1px solid red";
+    }
+}
+
+function confirmPassword(evt) {
+    var thisValue = evt.value;
+    
+    var passValue = document.getElementById("reg-password").value;
+    
+    if(thisValue === passValue) {
+        evt.style.border = "1px solid green";
+    } else {
+        evt.style.border = "1px solid red";
+    }
+}
+
 // show/hide registration form
 function overlay() {
-	el = document.getElementById("reg-container");
-	el.style.visibility = (el.style.visibility === "visible") ? "hidden" : "visible";
+    var el = document.getElementById("reg-container");
+
+    document.getElementById("reg-response").innerHTML = registration;
+
+    el.style.visibility = (el.style.visibility === "visible") ? "hidden" : "visible";
 }
 
 function signUp() {
@@ -26,22 +82,68 @@ function signUp() {
     var confirmPassword = document.getElementById("reg-confPword").value;
     
     xmlhttp.open("POST", "index.php/register", true);
-    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xmlhttp.send("uname=" + username + "&mail=" + mail + "&password=" + password + "&checkPassword=" + confirmPassword);
     
-    
-    
-    xmlhttp.onreadystatechange=function()
-    {
-        var result = JSON.parse(xmlhttp.responseText);
+    xmlhttp.onreadystatechange = function() {
+        var status = JSON.parse(xmlhttp.response);
         
-        if(result) {
-            document.getElementById("reg-response").innerHTML = "gilocav shen daregistrirdi!";
+        var msg = "";
+        
+        if(status === 1) {
+            msg = "<span style='color: red;'>Congratulations!</span> You have registered successfully<br>\n\
+                    <a href='#'>Click here</a> to sign in automatically<br>\n\
+                    Or <a onclick='overlay();' style='cursor: pointer;'>close</a> this form";
+        } else if(status === 2) {
+            msg = "<span style='color: red;'>Username is already taken.</span><br>\n\
+                    Please, <a onclick='this.parentNode.innerHTML = registration;' style='cursor: pointer;'>try again</a> or<br>\n\
+                    <a onclick='overlay();' style='cursor: pointer;'>Close this form</a>";
+        } else if(status === 3) {
+            msg = "<span style='color: red;'>Password did not match!</span><br>\n\
+                    Please, <a onclick='this.parentNode.innerHTML = registration;' style='cursor: pointer;'>try again</a> or<br>\n\
+                    <a onclick='overlay();' style='cursor: pointer;'>Close this form</a>";
         } else {
-            document.getElementById("reg-response").innerHTML = "Xelaxla scade! -_-";
+            msg = "<span style='color: red;'>Sorry... Something went wrong</span><br>\n\
+                    Please, <a onclick='this.parentNode.innerHTML = registration;' style='cursor: pointer;'>try again</a> or<br>\n\
+                    <a onclick='overlay();' style='cursor: pointer;'>Close this form</a>";
         }
+        
+        document.getElementById("reg-response").innerHTML = msg;
     };
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function signIn() {
+//    var xmlhttp = (new XMLHttpRequest) || (new ActiveXObject);
+//    
+//    var username = document.getElementById("sign-username").value;
+//    var password = document.getElementById("sign-password").value;
+//
+//    xmlhttp.open("POST", "index.php/sign", true);
+//    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+//    xmlhttp.send("uname=" + username + "&password=" + password);
+//    
+//    xmlhttp.onreadystatechange=function()
+//    {
+//        var result = JSON.parse(xmlhttp.responseText);
+//    };
+//}
+
+
 //    <!DOCTYPE html>
 //    <html>
 //    <head>
